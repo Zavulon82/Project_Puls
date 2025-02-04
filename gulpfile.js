96c49a6e0abb3fbbcf689dfea234706c776423bf
Zavulon82 <zavulon82@gmail.com>
2025-02-04 14:20:43 +0400

    change gulpfile 1

--- gulpfile.js
+++ gulpfile.js
@@ -17,17 +17,18 @@ gulp.task('server', function () {
 });
 
 gulp.task('styles', function () {
-    return gulp.src("src/css//*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest("src/css/dist"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
+
 gulp.task('watch', function () {
-    gulp.watch("src/css/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
