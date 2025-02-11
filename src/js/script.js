f024529c82dd07cacd19db43b94dfcdfb4670266
Zavulon82 <zavulon82@gmail.com>
2025-02-11 18:22:57 +0400

    js end lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
+var name = "Ivan";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth;
+
+
+
+
+
+
 // null
 // undefined
 // let obj = {
@@ -12,5 +26,51 @@
 // let answer = confirm("Вам есть 18?");
 // console.log(answer);
 
-let answer = prompt("Вам есть 18?", "");
-console.log(answer);
+// let answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// console.log(4 + 4);
+
+// console.log(4 + 'fdd');
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if (2 * 3 == 8 * 1) {
+//     console.log('Верно')
+// } else {
+//     console.log('Ошибка')
+// }
+
+// let answer = confirm("Вам есть 18?");
+// if (answer) {
+//     console.log('Проходите')
+// } else {
+//     console.log('Уходи')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Неправильно')
+// } else if (num > 100) {
+//     console.log('Много')
+// } else {
+//     console.log('Верно')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging(3, 5);
+logging(3, 9);
+logging(7, 9);
