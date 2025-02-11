8c550fd6048197870be8947e9bfeac0c59bf3186
Zavulon82 <zavulon82@gmail.com>
2025-02-11 16:29:04 +0400

    js

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// null
+// undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+// Symbol
+
+// alert(1234)
+// console.log(nullmber);
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+let answer = prompt("Вам есть 18?", "");
+console.log(answer);
